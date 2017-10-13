52dc6fef4de0a5f5cd911edfc4fd3cc53697978b
Maksym Dmukhovskyy <maksymdmukhovskyy@MacBook-Air-di-Maksym.local>
2017-10-13 21:40:04 +0100

    Removing starting screen adding functionality;

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 });
 
 var coinAPI = [];
-var crypt = 0;
-var cur = 0;
+var crypt = "BTC";
+var cur = "USD";
 
 var priceBTN = $('.quickPrice');
 var convBTN = $('.converter');
@@ -13,6 +13,13 @@ var popUpCard = $('.popUpCard');
 var submitBTN = $('.submit');
 var cryptoPrice = $('#cryptoCurPri');
 var curPrice = $('#curPri');
+
+var preloader = $('.preloader-wrapper');
+var value = $('.value');
+var percent = $('.percent');
+var dataWrap = $('.dataWrap');
+
+var loading = false;
 
 function hidePopup() {
   popUpCard.fadeTo(400, 0, function(){
@@ -26,7 +33,10 @@ function getCoinPrice(crypt, cur) {
       type: 'POST',
       data: {},
       datatype: 'json',
-      success: function(data) { coinAPI = data },
+      success: function(data) { 
+        coinAPI = JSON.parse(data);
+        displayResult(coinAPI);
+      },
       error: function(err) { alert(err); },
       beforeSend: function(xhr) {
       xhr.setRequestHeader("X-Mashape-Authorization", "7eZ7NbbmF6mshfvBhQuXUdqgaAclp17w3hsjsnX2QSB8J7M6Pi");
@@ -38,13 +48,45 @@ function getConvertion() {
 
 }
 
+function displayResult(coinAPI) {
+  valueText = Number(coinAPI.last_price).toFixed(3);
+  percentVal = coinAPI.vol_24hr_pcnt;
+  percentState = 0;
+  if (percentVal.charAt(0) != '-') {
+    percentState = '+ ';
+  } else {
+    percentState = '';
+    };
+  value.text(cur + ' ' + valueText);
+  percent.text(percentState + ' ' +percentVal + '%');
+  loading = false;
+  preloader.fadeTo(500, 0, function(){
+    preloader.hide();
+    dataWrap.show;
+    dataWrap.fadeTo(500, 1);
+  });
+}
+
 priceBTN.click(function(e) {
   hidePopup();
 })
 
 submitBTN.click(function(e) {
-  crypt = cryptoPrice.val();
-  cur = curPrice.val();
-  getCoinPrice(crypt, cur);
+    if (!loading && crypt !== cryptoPrice.val() || cur !== curPrice.val()) {
+      crypt = cryptoPrice.val();
+      cur = curPrice.val();
+      getCoinPrice(crypt, cur);
+      dataWrap.fadeTo(500, 0, function() {
+        dataWrap.hide();
+        preloader.show();
+        preloader.fadeTo(500, 1);
+      })
+      loading = true;
+    }
 })
 
+window.setTimeout(function() {
+  getCoinPrice(crypt, cur);
+  preloader.show();
+  preloader.fadeTo(500, 1);
+}, 500);
